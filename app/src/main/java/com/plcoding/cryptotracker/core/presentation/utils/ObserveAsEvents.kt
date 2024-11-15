02455efd277f9a9fc61c342084c5982a0a80b2d7
Given Tshuma <given.tshuma@pmconnect.co.uk>
2024-11-15 10:22:33 +0200

    remove unused context variable

--- app/src/main/java/com/plcoding/cryptotracker/core/presentation/utils/ObserveAsEvents.kt
+++ app/src/main/java/com/plcoding/cryptotracker/core/presentation/utils/ObserveAsEvents.kt
@@ -1,13 +1,10 @@
 package com.plcoding.cryptotracker.core.presentation.utils
 
-import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
-import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListEvent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
@@ -19,7 +16,6 @@ fun <T> ObserveAsEvents(
     key2: Any? =null,
     onEvent: (T) -> Unit
 ) {
-    val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     LaunchedEffect(lifecycleOwner.lifecycle,key1,key2) {
         lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
